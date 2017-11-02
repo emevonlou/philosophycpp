0184ac27d776c8d475f81a6010dc434564b8a485
Emanuelle Costa <eme@vonlou.dev>
2017-11-02 15:54:43 -0300

    randphilosophy

---
+++ 05.cpp
@@ -0,0 +1,34 @@
+#include <iostream> 
+#include <cstdlib> 
+#include <time.h> 
+#include <conio.h> 
+
+using namespace std;
+
+int main() {
+	srand(time(NULL));
+	int x, y[20];
+	x = rand() % 10;
+	
+	cout << x << endl << endl;
+	system("pause");
+	
+	for(int i =0; i < 20; i++)
+    {
+		y[i] = rand() %10;
+		cout << y[i] << " ";
+	}
+	
+	cout << endl;
+	system("pause");
+    system("cls");
+    
+    for(int i =0;i != 27;) 
+	{
+    	for(int j =0; j < 10; j++) 
+		{
+		y[i] = rand() %10;
+		cout << "Filosofia Niet" + y[i] << " ";
+		}
+    }
+}
