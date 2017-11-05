a416b9de6db1063cd2ed97bc58e6bad695b70976
Emanuelle Costa <eme@vonlou.dev>
2017-11-05 17:58:56 -0300

    books1

--- 08.cpp
+++ 08.cpp
@@ -14,10 +14,12 @@ int main()
     char x;
 
     cout << "sobre os livros de Nietzsche para cada alternativa digite: " << endl;
+    cout << "---------------------------------------------------------------------" << endl;
     cout << "(a) Se o trecho se referir ao Livro Aurora." << endl;
-    cout << "(w) se o trecho se referir ao livro um problema para músicos." << endl;
+    cout << "(w) se o trecho se referir ao livro um problema para musicos." << endl;
     cout << "(z) Se o trecho se referir ao livro Assim Falou Zaratustra." << endl;
     cout << "(b) Se o trecho se referir ao livro Wagner em Bayreuth." << endl;
+    cout << "----------------------------------------------------------------------" << endl;
 
     cout << "Como nos fazem bem as tardes bronzeas da sua felicidade ";
     cout << "Olhamos pra fora ao ouvi-la: Ja vimos o mar tao liso? ";
@@ -33,7 +35,7 @@ int main()
     }
     else
     {
-        cout << "Noopz.Esse trecho é do livro 'Um problema para musicos'(pág.13)" << endl;
+        cout << "Noopz.Esse trecho eh do livro 'Um problema para musicos'(pág.13)" << endl;
     }
 
     cout << "----------------------------------------------------------------------------" << endl;
@@ -44,13 +46,48 @@ int main()
 
     if(x == a)
     {
-        cout << "Bravo!!Hoje nasceu uma aurora!";
+        cout << "Bravo!!Hoje nasceu uma aurora!" << endl;
     }
     else
     {
-        cout << "Noopz. Esse trecho foi retirado do livro 'Aurora'(p.75)!";
+        cout << "Noopz. Esse trecho foi retirado do livro 'Aurora'(p.75)!" << endl;
     }
-    
+
+    cout << "-----------------------------------------------------------------------------" << endl;
+    cout << "Nenhum pastor e um so rebanho! Todos querem o mesmo, ";
+    cout << "todos sao iguais: O que pensa de outro modo tende a ir ";
+    cout << "para o manicomio.";
+    cout << "Digite o caracter correspondente ao livro que cita esse trecho: " << endl;
+    cin >> x;
+
+    if(x == z)
+    {
+        cout << "YESS!Parabens! Es um dos que participa efetivamente do ";
+        cout << "preambulo de zaratustra!" << endl;
+    }
+    else
+    {
+        cout << "Noopz! Esse trecho foi retirado do livro 'Assim falou zaratustra'";
+        cout << "Da primeira parte do preambulo(p.29)" << endl;
+    }
+
+    cout << "------------------------------------------------------------------------------" << endl;
+    cout << "A Liberdade nao eh algo que recebemos como se fosse uma dadiva ";
+    cout << "o homem livre se faz por si mesmo ao se confrontar com perguntas ";
+    cout << "sobre o sentido de sua existencia e, perceber que ninguem, a nao ser ";
+    cout << "ele proprio, pode responde-las!" << endl;
+    cout << "Digite o caracter correspondente ao livro que cita esse trecho: " << endl;
+    cin >> x;
+
+    if(x == b)
+    {
+        cout << "Em Bayreuth e em qualquer lugar voce estara certo!" << endl;
+    }
+    else
+    {
+        cout << "Esse trecho foi retirado do livro Wagner em Bayreuth(p.25)." << endl;
+    }
+
     system("pause");
 return 0;
 }
