5d213c0b40053baeb9d879ae180d56030ba2ab09
Emanuelle Costa <eme@vonlou.dev>
2017-11-17 15:07:20 -0300

    Nota para uma Dinamite!

--- 09.cpp
+++ 09.cpp
@@ -3,26 +3,27 @@ using namespace std;
 
 int main()
 {
+   int x = 0;
    int n;
-   cout << "Que nota você atribuiria ao filósofo Nietzsche(de 1 a 10)? " << endl;
+   cout << "Que nota voce atribuiria ao filosofo Nietzsche(de 1 a 10)? " << endl;
    cin >> n;
    if(n <= 5)
    {
-   do
-   {
-       cout << "De toda forma, ele foi uma dinamite. ";
-       break;
-   }
-   while(n <= 10);
+        do
+        {
+            cout << "De toda forma, ele foi uma dinamite. " << endl;
+            x = x + 1;
+        }
+        while(x <= n);
    }
    else
    {
-    do
-    {
-        cout << "BRAVO!!";
-        break;
-    }
-    while(n >= 10);
+        do
+        {
+            cout << "BRAVO!!" << endl;
+            n = n + 1;
+        }
+        while(x <= n);
    }
    return 0;
 }
