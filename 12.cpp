2697f0bf4720ead89aa489ce0d51b5edc7b26c21
Emanuelle Frota <emanuellebc@gmail.com>
2017-11-18 14:59:10 -0300

    Uma simples Aurora Niilista

---
+++ 12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Na lib da minha vida,Vinculo a variante da biblioteca ";
+    cout << "estatica a um tempo de execucao no futuro.E no colo,o inesperado.";
+    cout << "Declaracoes de espacos entre uma essencia e outra,";
+    cout << "consistindo numa eternidade onde o tempo ";
+    cout << "eh sempre postumo por amor ao presente. ";
+    cout << "Na minha funcao principal,que tudo retorne sempre inteiro." << endl;
+
+    return 0;
+}
--- Makefile
+++ Makefile
@@ -10,4 +10,5 @@ build:
 	g++ -c -o 09.o 09.cpp; g++ -o 09 09.o
 	g++ -c -o 10.o 10.cpp; g++ -o 10 10.o
 	g++ -c -o 11.o 11.cpp; g++ -o 11 11.o
+	g++ -c -o 12.o 12.cpp; g++ -o 12 12.o
 .PHONY:
