d87e429512351c3ff2ba9c4d4c48a9744564fa1b
Emanuelle Frota <emanuellebc@gmail.com>
2017-11-20 14:43:03 -0300

    mais uma consideração extemporânea

--- 12.cpp
+++ 12.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
+#include <locale.h>
 using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "");
+
     cout << "Na lib da minha vida,Vinculo a variante da biblioteca ";
     cout << "estatica a um tempo de execucao no futuro.E no colo,o inesperado.";
     cout << "Declaracoes de espacos entre uma essencia e outra,";
     cout << "consistindo numa eternidade onde o tempo ";
     cout << "eh sempre postumo por amor ao presente. ";
     cout << "Na minha funcao principal,que tudo retorne sempre inteiro." << endl;
+
+    cout << "---------------------------------------------------------------------------------------" << endl;
+
+    cout << "quando a inspiração me vem no colo, " << endl;
+    cout << "o que a representa fica bem alí," << endl;
+    cout << "por entre espaços duros do coração," << endl;
+    cout << "de forma totalmente irresponsável." << endl;
+    cout << "pulsando eletricamente em alguma verdade que vez por outra" << endl;
+    cout << "parece cantar." << endl;
 
     return 0;
 }
