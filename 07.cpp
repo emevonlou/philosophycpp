61014046a11d43bceb92e990ec31eb68edd8d54f
emevonlou <emanuellebc@gmail.com>
2018-01-20 15:32:20 -0300

    update

--- 07.cpp
+++ 07.cpp
@@ -6,10 +6,9 @@ int main(int argc, char *argv[])
 {
     setlocale(LC_ALL, "");
 
-    
-    for(int i = 0; i < 5; i++)
-	{
-    
+    for (int i = 0; i < 5; i++)
+    {
+
         cout << "Heráclito!" << endl;
         cout << "Shopenhauer!" << endl;
         cout << "Lou Salomé!" << endl;
@@ -17,22 +16,23 @@ int main(int argc, char *argv[])
         cout << "Schiler!" << endl;
         cout << "Richard Wagner!" << endl;
         cout << "Georges Bizet!" << endl;
-        cout << "Peter gast" << endl; 
+        cout << "Peter gast" << endl;
         cout << "São Inspirações Nietzscheanas!" << endl;
-	}   
-	
-	cout << endl << endl;
-	cout << "Anti inspirações Nietzscheanas!" << endl;
+    }
+
+    cout << endl
+         << endl;
+    cout << "Anti inspirações Nietzscheanas!" << endl;
     cout << "Kant" << endl;
     cout << "Parmenides" << endl;
     cout << "Hegel" << endl;
     cout << "Richard Wagner!" << endl;
-    cout << endl << endl;
+    cout << endl
+         << endl;
     cout << "P.S: Nietzsche sofreu uma séria decepção ";
     cout << "Com Richard Wagner, quando percebeu o nome ";
     cout << "Dele ligado ao antisemitismo e outros ";
     cout << "preconceitos da época.";
 
-  
     return 0;
 }
