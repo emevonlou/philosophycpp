1de90706e96f38629b73ee85809f016ea11f0c25
Emanuelle Costa <eme@vonlou.dev>
2018-01-20 15:33:25 -0300

    update

--- 09.cpp
+++ 09.cpp
@@ -3,27 +3,25 @@ using namespace std;
 
 int main()
 {
-   int x = 0;
-   int n;
-   cout << "Que nota voce atribuiria ao filosofo Nietzsche(de 1 a 10)? " << endl;
-   cin >> n;
-   if(n <= 5)
-   {
+    int x = 0;
+    int n;
+    cout << "Que nota voce atribuiria ao filosofo Nietzsche(de 1 a 10)? " << endl;
+    cin >> n;
+    if (n <= 5)
+    {
         do
         {
             cout << "De toda forma, ele foi uma dinamite. " << endl;
             x = x + 1;
-        }
-        while(x <= n);
-   }
-   else
-   {
+        } while (x <= n);
+    }
+    else
+    {
         do
         {
             cout << "BRAVO!!" << endl;
             n = n + 1;
-        }
-        while(x <= n);
-   }
-   return 0;
+        } while (x <= n);
+    }
+    return 0;
 }
