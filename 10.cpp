a71430c91ed5cefacb0b7e754dfa054900a97a24
emevonlou <emanuellebc@gmail.com>
2018-01-20 15:33:58 -0300

    update

--- 10.cpp
+++ 10.cpp
@@ -4,19 +4,20 @@ using namespace std;
 int main()
 {
     int x;
-    cout << "Summum bonum ( 'sumo bem' ou 'bem maior, em latim) eh uma expressao latina usada na filosofia - particularmente, em Aristoteles "; 
-    cout << ",na filosofia medieval e na filosofia de Immanuel Kant - para descrever o bem maior que o ser humano deve buscar." << endl << endl;
+    cout << "Summum bonum ( 'sumo bem' ou 'bem maior, em latim) eh uma expressao latina usada na filosofia - particularmente, em Aristoteles ";
+    cout << ",na filosofia medieval e na filosofia de Immanuel Kant - para descrever o bem maior que o ser humano deve buscar." << endl
+         << endl;
     cout << "Voce se considera sujeito ao 'Summum Bonum'(digite 1 pra sim e 0 pra nao)?" << endl;
     cin >> x;
 
-        if(x == true)
-        {
-            cout << "Parabens.Summum Bonum eh o maximo do bem que eh um fim em si.";
-        }
-        else
-        {
-            cout << "Nada mal. A natureza desse estado preocupou muitos autores, dentre eles cristaos.";
-        }
+    if (x == true)
+    {
+        cout << "Parabens.Summum Bonum eh o maximo do bem que eh um fim em si.";
+    }
+    else
+    {
+        cout << "Nada mal. A natureza desse estado preocupou muitos autores, dentre eles cristaos.";
+    }
 
     return 0;
 }
