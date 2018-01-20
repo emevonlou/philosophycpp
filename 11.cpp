7377f3ddb8a975dd8cf1850b68cf89673226e8d2
emevonlou <emanuellebc@gmail.com>
2018-01-20 15:34:23 -0300

    update

--- 11.cpp
+++ 11.cpp
@@ -8,23 +8,23 @@ int main()
     cout << "Qual o dia em que nietzsche nasceu?";
     cin >> x;
 
-    switch(x)
+    switch (x)
     {
-        case 15:
-           cout << "YES! No dia 15 de outubro de 1844 nasceu a Aurora niilista dual! um espetaculo." << endl;
-           break;
-        case 10:
-            cout << "Cinco dias a mais e voce estaria correto. Nietzsche nasceu dia 15 de outubro de 1844." << endl;
-            break;
-        case 20:
-            cout << "Cinco dias a menos e voce estaria correto. Nieyzsche nasceu dia 15 de outubro de 1944." << endl;
-            break;
-        case 30:
-            cout << "Quinze dias a menos e voce estaria correto. Nietzsche nasceu dia 15 de outubro de 1944." << endl;
-            break;
-        default:
-            cout << "Sorry, tente novamente." << endl;
-            break;
+    case 15:
+        cout << "YES! No dia 15 de outubro de 1844 nasceu a Aurora niilista dual! um espetaculo." << endl;
+        break;
+    case 10:
+        cout << "Cinco dias a mais e voce estaria correto. Nietzsche nasceu dia 15 de outubro de 1844." << endl;
+        break;
+    case 20:
+        cout << "Cinco dias a menos e voce estaria correto. Nieyzsche nasceu dia 15 de outubro de 1944." << endl;
+        break;
+    case 30:
+        cout << "Quinze dias a menos e voce estaria correto. Nietzsche nasceu dia 15 de outubro de 1944." << endl;
+        break;
+    default:
+        cout << "Sorry, tente novamente." << endl;
+        break;
     }
 
     return 0;
