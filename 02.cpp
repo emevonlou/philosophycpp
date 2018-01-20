94604112693740b622ba97e8084f56889f88578d
Emanuelle Costa <eme@vonlou.dev>
2018-01-20 15:29:12 -0300

    update

--- 02.cpp
+++ 02.cpp
@@ -7,15 +7,15 @@ int main()
 {
     setlocale(LC_ALL, "");
     int x;
-    
+
     cout << "Você acredita que é capaz de amar o destino que escolheu?" << endl;
     cout << "Digite 1 para sim e 0 para não." << endl;
     cin >> x;
 
-    if(x == 1)
+    if (x == 1)
     {
-       cout << "Friedrich Nietzsche concorda com você." << endl;
-       cout << "Para uma reafirmação disso, leia 'Além do bem e do mal'." << endl; 
+        cout << "Friedrich Nietzsche concorda com você." << endl;
+        cout << "Para uma reafirmação disso, leia 'Além do bem e do mal'." << endl;
     }
     else
     {
