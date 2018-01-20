c6c90a5131d69c6bcfd44d306a4185a18752e23f
emevonlou <emanuellebc@gmail.com>
2018-01-20 15:30:56 -0300

    update

--- 05.cpp
+++ 05.cpp
@@ -1,34 +1,36 @@
-#include <iostream> 
-#include <cstdlib> 
-#include <time.h> 
-#include <conio.h> 
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+// #include <conio.h>
 
 using namespace std;
 
-int main() {
+int main()
+{
 	srand(time(NULL));
 	int x, y[20];
 	x = rand() % 10;
-	
-	cout << x << endl << endl;
+
+	cout << x << endl
+		 << endl;
 	system("pause");
-	
-	for(int i =0; i < 20; i++)
-    {
-		y[i] = rand() %10;
+
+	for (int i = 0; i < 20; i++)
+	{
+		y[i] = rand() % 10;
 		cout << y[i] << " ";
 	}
-	
+
 	cout << endl;
 	system("pause");
-    system("cls");
-    
-    for(int i =0;i != 27;) 
+	system("cls");
+
+	for (int i = 0; i != 27;)
 	{
-    	for(int j =0; j < 10; j++) 
+		for (int j = 0; j < 10; j++)
 		{
-		y[i] = rand() %10;
-		cout << "Filosofia Niet" + y[i] << " ";
+			y[i] = rand() % 10;
+			cout << "Filosofia Niet" + y[i] << " ";
 		}
-    }
+	}
 }
