269afdc7f6a18d31718fd96b08cbb2a85f4b327e
Emanuelle Costa <eme@vonlou.dev>
2018-01-22 15:18:26 -0300

    update

---
+++ 14.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    const char s = 's';
+    const char n = 'n';
+    char w;
+
+    cout << "Voce acredita que o humano nasce da alteridade(s/n)?" << endl;
+    cin >> w;
+
+    if (w == s)
+    {
+        cout << "sIM!Para o grande filósofo Emanuel Levinas, a ALTERIDADE,\n";
+        cout << "o comeco do filosofar, o fundamento da razao, o sentido\n";
+        cout << "do humano eh a possibilidade de realização da justica e paz!!\n";
+    }
+    else if (w == n)
+    {
+        cout << "Voce precisa estudar o grande filosofo judeu lituano\n";
+        cout << "Emanuel Levinas!" << endl;
+    }
+    else
+    {
+        cout << "Opcao invalida.Tente outra vez\n";
+    }
+
+    return 0;
+}
