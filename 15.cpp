665e5cf12df11ff21b26c48c39e3fa8c634b49f2
emevonlou <emanuellebc@gmail.com>
2018-01-23 10:27:45 -0300

    Testing

--- 15.cpp
+++ 15.cpp
@@ -6,29 +6,27 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    const char *validchars = "EmanuelLevinas!";
-    char *novastr;
+    const char *levinas = "E,m,a,n,u,e,l,L,e,v,i,n,a,s!";
+    char *newstr;
 
-    int str_len;
+    int tamstr;
 
-srand(time(NULL));
+    srand(time(NULL));
 
+    // Tamanho da string.
+    tamstr = (rand() % 30);
 
-// tamanho da string
-str_len = (rand() % 30 );
+    // se for menor que cinco, configura para ser maior que cinco.
+    tamstr += tamstr < 5 ? 5 : 0;
 
-// se for menor que cinco, configura para ser maior que cinco
-str_len += str_len < 5 ? 5 : 0;
+    // aloca memoria
+    newstr = (char *)malloc((tamstr + 1) * sizeof(char));
 
-
-// aloca memoria
-novastr = (char*)malloc((str_len + 1)* sizeof(char));
-
-
-for ( int i = 0; i < str_len; i++ ) {
-    novastr[i] = validchars[ rand() % strlen(validchars) ];
-    novastr[i + 1] = 0x0;
-    cout << *validchars << endl;
-}
+    for (int i = 0; i < tamstr; i++)
+    {
+        newstr[i] = levinas[rand() % strlen(levinas)];
+        newstr[i + 1] = 0x0;
+        cout << *levinas << endl;
+    }
     return 0;
 }
