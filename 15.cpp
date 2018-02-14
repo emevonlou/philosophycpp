2a9b8d568a37365f1ccee42c44d75c6ffedd0d50
emevonlou <emanuellebc@gmail.com>
2018-02-14 16:30:02 -0300

    Adicionando cores!

--- 15.cpp
+++ 15.cpp
@@ -1,32 +1,14 @@
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-using namespace std;
-
-int main(int argc, char *argv[])
+ 
+int numeroAleatorio(int menor, int maior) {
+       return rand()%(maior-menor+1) + menor;
+}
+int main (int argc, char const* argv[])
 {
-    const char *levinas = "E,m,a,n,u,e,l,L,e,v,i,n,a,s!";
-    char *newstr;
-
-    int tamstr;
-
-    srand(time(NULL));
-
-    // Tamanho da string.
-    tamstr = (rand() % 30);
-
-    // se for menor que cinco, configura para ser maior que cinco.
-    tamstr += tamstr < 5 ? 5 : 0;
-
-    // aloca memoria
-    newstr = (char *)malloc((tamstr + 1) * sizeof(char));
-
-    for (int i = 0; i < tamstr; i++)
-    {
-        newstr[i] = levinas[rand() % strlen(levinas)];
-        newstr[i + 1] = 0x0;
-        cout << *levinas << endl;
-    }
-    return 0;
+srand((unsigned)time(0)); //para gerar números aleatórios reais.
+int aleatorio = numeroAleatorio(30, 100);
+std::cout << "Numero Aleatorio = " << aleatorio << std::endl;
+return 0;
 }
