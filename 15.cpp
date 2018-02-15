6959af80eee333bfc227a55f69fbe6b78ae7f6ef
emevonlou <emanuellebc@gmail.com>
2018-02-15 17:27:17 -0300

    rand

--- 15.cpp
+++ 15.cpp
@@ -1,14 +1,58 @@
-#include <cstdlib>
-#include <ctime>
-#include <iostream>
- 
-int numeroAleatorio(int menor, int maior) {
-       return rand()%(maior-menor+1) + menor;
-}
-int main (int argc, char const* argv[])
+// 9:
+#include <iostream> // cin, cout
+#include <cstdlib>  // System,random(rand).
+#include <time.h>   // Time.
+//#include <conio.h>  // Funções do windows.
+
+using namespace std;
+
+int main()
 {
-srand((unsigned)time(0)); //para gerar números aleatórios reais.
-int aleatorio = numeroAleatorio(30, 100);
-std::cout << "Numero Aleatorio = " << aleatorio << std::endl;
-return 0;
+    char eme[] = {0x1b, '[', '1', ';', '3', '5', 'm', 0};
+	cout << eme;
+	srand(time(NULL));
+	int a, b[20];
+	a = rand() % 10;
+
+	cout << a << endl
+		 << endl;
+	system("pause");
+
+	for (int i = 0; i < 20; i++)
+	{
+		b[i] = rand() % 10;
+		cout << b[i] << " ";
+	}
+	cout << endl;
+	system("pause");
+	system("cls");
+
+	for (int i = 0; a != 27;)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			b[i] = rand() % 10;
+			cout << b[i] << " ";
+		}
+		// a = getch(); // Verifica valor randomico pausadamente.
+		if (a == 97)
+		{
+			b[i] = rand() % 100 + 1;
+			i++;
+			if (i == 9)
+			{
+				i = 0;
+			}
+		}
+		else if (a == 100)
+		{
+			b[i] = rand() % 100 + 1;
+			i--;
+			if (i == -1)
+			{
+				i = 9;
+			}
+			system("cls");
+		}
+	}
 }
